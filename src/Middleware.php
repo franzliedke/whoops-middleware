153abbc3ce6327eba609ff8754eb913d1c8a770a
Franz Liedke <franz@develophp.org>
2015-10-31 12:10:43 +0100

    Extract Whoops handler logic to separate class

--- src/Middleware.php
+++ src/Middleware.php
@@ -4,9 +4,6 @@ namespace Franzl\Middleware\Whoops;
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
-use Whoops\Handler\PrettyPageHandler;
-use Whoops\Run;
-use Zend\Diactoros\Response\StringResponse;
 
 class Middleware
 {
@@ -15,16 +12,6 @@ class Middleware
      */
     public function __invoke($error, Request $request, Response $response, callable $out = null)
     {
-        $whoops = new Run();
-        $whoops->pushHandler(new PrettyPageHandler());
-        $whoops->register();
-
-        $method = Run::EXCEPTION_HANDLER;
-
-        ob_start();
-        $whoops->$method($error);
-        $response = ob_get_clean();
-
-        return StringResponse::html($response, 500);
+        return WhoopsRunner::handle($error);
     }
 }
