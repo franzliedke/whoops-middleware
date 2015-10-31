4a5d320a20865870c796a80a01e6e76ddc8d03b2
Franz Liedke <franz@develophp.org>
2015-10-31 12:12:36 +0100

    Rename Middleware class to ErrorMiddleware
    We'll have another classical middleware soon, and this is better
    suited to match Stratigility's strategy of separate error middlewares
    for handling exceptions.

--- src/ErrorMiddleware.php
+++ src/ErrorMiddleware.php
@@ -5,11 +5,11 @@ namespace Franzl\Middleware\Whoops;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
-class Middleware
+/**
+ * ErrorMiddleware class for use with Zend's Stratigility middleware pipe
+ */
+class ErrorMiddleware
 {
-    /**
-     * {@inheritdoc}
-     */
     public function __invoke($error, Request $request, Response $response, callable $out = null)
     {
         return WhoopsRunner::handle($error);
