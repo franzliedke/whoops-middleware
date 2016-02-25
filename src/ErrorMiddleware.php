f4eea73844e7c089e3adc4567718667cd972795e
David Négrier <d.negrier@thecodingmachine.com>
2016-02-25 18:59:53 +0100

    Adding (needed) Stratigility interface

--- src/ErrorMiddleware.php
+++ src/ErrorMiddleware.php
@@ -4,11 +4,12 @@ namespace Franzl\Middleware\Whoops;
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use Zend\Stratigility\ErrorMiddlewareInterface;
 
 /**
  * ErrorMiddleware class for use with Zend's Stratigility middleware pipe
  */
-class ErrorMiddleware
+class ErrorMiddleware implements ErrorMiddlewareInterface
 {
     public function __invoke($error, Request $request, Response $response, callable $out = null)
     {
