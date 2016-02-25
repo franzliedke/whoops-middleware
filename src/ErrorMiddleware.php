9bce6476b020e07086fb1a60763d150170f0f10c
David Négrier <d.negrier@thecodingmachine.com>
2016-02-25 19:19:07 +0100

    Changing arity of ErrorMiddleware to match Stratigility expectation

--- src/ErrorMiddleware.php
+++ src/ErrorMiddleware.php
@@ -4,14 +4,13 @@ namespace Franzl\Middleware\Whoops;
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
-use Zend\Stratigility\ErrorMiddlewareInterface;
 
 /**
  * ErrorMiddleware class for use with Zend's Stratigility middleware pipe
  */
-class ErrorMiddleware implements ErrorMiddlewareInterface
+class ErrorMiddleware
 {
-    public function __invoke($error, Request $request, Response $response, callable $out = null)
+    public function __invoke($error, Request $request, Response $response, callable $out)
     {
         return WhoopsRunner::handle($error);
     }
