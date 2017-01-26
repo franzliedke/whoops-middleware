47c18c1ca85b2bc9b5ad4d14c2f2d8146fb9e800
Tim Tegeler <tim@tegeler.me>
2017-01-26 11:14:01 +0100

    Approach to support psr15 ^0.4

---
+++ src/PSR15Middleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Franzl\Middleware\Whoops;
+
+use Interop\Http\ServerMiddleware\DelegateInterface;
+use Interop\Http\ServerMiddleware\MiddlewareInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * Middleware class for PSR-15 middleware
+ */
+class PSR15Middleware implements MiddlewareInterface
+{
+
+    /**
+     * @param ServerRequestInterface $request
+     * @param DelegateInterface $delegate
+     *
+     * @return ResponseInterface
+     */
+    public function process(ServerRequestInterface $request, DelegateInterface $delegate)
+    {
+        try {
+            return $delegate->process($request);
+        } catch (\Exception $e) {
+            return WhoopsRunner::handle($e, $request);
+        }
+    }
+}
