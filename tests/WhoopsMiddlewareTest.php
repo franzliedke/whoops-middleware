a03b867ee8d7ffe5baf444ede164190db47d0047
Hasham Ahmad <mr.hash@gmail.com>
2020-06-30 16:05:18 -0500

    Use response factory

--- tests/WhoopsMiddlewareTest.php
+++ tests/WhoopsMiddlewareTest.php
@@ -4,35 +4,64 @@ namespace Franzl\Middleware\Whoops\Test;
 
 use Exception;
 use Franzl\Middleware\Whoops\WhoopsMiddleware;
+use Middlewares\Utils\Factory;
+use Middlewares\Utils\FactoryInterface;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\StreamInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Zend\Diactoros\Response\TextResponse;
-use Zend\Diactoros\ServerRequest;
 
 class WhoopsMiddlewareTest extends TestCase
 {
+    public function setUpForError($expectedContentType)
+    {
+        $responseMock = $this->createMock(ResponseInterface::class);
+        $responseMock->expects($this->once())->method('withBody')->willReturnSelf();
+        $responseMock->expects($this->once())
+            ->method('withHeader')
+            ->with('Content-Type', $expectedContentType)
+            ->willReturnSelf();
+        $responseFactoryMock = $this->createMock(ResponseFactoryInterface::class);
+        $responseFactoryMock->expects($this->once())->method('createResponse')->with(500)->willReturn($responseMock);
+
+        $streamMock = $this->createMock(StreamInterface::class);
+        $streamFactoryMock = $this->createMock(StreamFactoryInterface::class);
+        $streamFactoryMock->expects($this->once())->method('createStream')->willReturn($streamMock);
+
+        $factoryMock = $this->createMock(FactoryInterface::class);
+        $factoryMock->expects($this->once())->method('getResponseFactory')->willReturn($responseFactoryMock);
+        $factoryMock->expects($this->once())->method('getStreamFactory')->willReturn($streamFactoryMock);
+
+        /** @var FactoryInterface $factoryMock */
+        Factory::setFactory($factoryMock);
+    }
+
     public function test_successful_request_is_left_untouched()
     {
+        $requestMock = $this->createMock(ServerRequestInterface::class);
+        $responseMock = $this->createMock(ResponseInterface::class);
+        /**
+         * @var ServerRequestInterface $requestMock
+         * @var ResponseInterface $responseMock
+         */
         $response = (new WhoopsMiddleware)->process(
-            new ServerRequest,
-            $this->handlerThatReturns(new TextResponse('Success!'))
+            $requestMock,
+            $this->handlerThatReturns($responseMock)
         );
 
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals('Success!', $response->getBody());
+        $this->assertSame($responseMock, $response);
     }
 
     public function test_exception_is_handled()
     {
-        $response = (new WhoopsMiddleware)->process(
-            new ServerRequest,
+        $this->setUpForError('text/html');
+        (new WhoopsMiddleware)->process(
+            $this->requestWithAccept('text/html'),
             $this->handlerThatThrowsException()
         );
-
-        $this->assertEquals(500, $response->getStatusCode());
-        $this->assertEquals('text/html', $response->getHeaderLine('content-type'));
     }
 
     /**
@@ -40,13 +69,11 @@ class WhoopsMiddlewareTest extends TestCase
      */
     public function test_known_mime_types_will_return_preferred_content_type($mime, $expectedContentType)
     {
-        $response = (new WhoopsMiddleware)->process(
+        $this->setUpForError($expectedContentType);
+        (new WhoopsMiddleware)->process(
             $this->requestWithAccept($mime),
             $this->handlerThatThrowsException()
         );
-
-        $this->assertEquals(500, $response->getStatusCode());
-        $this->assertEquals($expectedContentType, $response->getHeaderLine('content-type'));
     }
 
     public function knownTypes()
@@ -64,33 +91,31 @@ class WhoopsMiddlewareTest extends TestCase
 
     public function test_multiple_mime_types_will_prefer_the_first_match()
     {
-        $response = (new WhoopsMiddleware)->process(
+        $this->setUpForError('text/xml');
+        (new WhoopsMiddleware)->process(
             $this->requestWithAccept('application/xml, application/json'),
             $this->handlerThatThrowsException()
         );
+    }
 
-        $this->assertEquals(500, $response->getStatusCode());
-        $this->assertEquals('text/xml', $response->getHeaderLine('content-type'));
-
+    public function test_multiple_mime_types_will_prefer_the_first_match_reversed()
+    {
         // Test vice versa to avoid false positives
-        $response = (new WhoopsMiddleware)->process(
+        $this->setUpForError('application/json');
+        (new WhoopsMiddleware)->process(
             $this->requestWithAccept('application/json, application/xml'),
             $this->handlerThatThrowsException()
         );
-
-        $this->assertEquals(500, $response->getStatusCode());
-        $this->assertEquals('application/json', $response->getHeaderLine('content-type'));
     }
 
     public function test_unknown_mime_types_will_fall_back_to_plain_text()
     {
-        $response = (new WhoopsMiddleware)->process(
+        $this->setUpForError('text/plain');
+
+        (new WhoopsMiddleware)->process(
             $this->requestWithAccept('foo/bar, x/custom'),
             $this->handlerThatThrowsException()
         );
-
-        $this->assertEquals(500, $response->getStatusCode());
-        $this->assertEquals('text/plain', $response->getHeaderLine('content-type'));
     }
 
     private function handlerThatReturns(ResponseInterface $response)
@@ -114,8 +139,14 @@ class WhoopsMiddlewareTest extends TestCase
         };
     }
 
+    /**
+     * @return ServerRequestInterface
+     */
     private function requestWithAccept($acceptHeader)
     {
-        return (new ServerRequest)->withHeader('accept', $acceptHeader);
+        $headers = explode(',', $acceptHeader);
+        $requestMock = $this->createMock(ServerRequestInterface::class);
+        $requestMock->expects($this->once())->method('getHeader')->with('accept')->willReturn($headers);
+        return $requestMock;
     }
 }
