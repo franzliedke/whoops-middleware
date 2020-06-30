71d75c5fff75587d6194a051d510a9eca0e3a047
Hasham Ahmad <mr.hash@gmail.com>
2020-06-30 16:05:18 -0500

    Use response factory from middlewares/utils
    Diactoros is no longer required and demoted to a dev dependency, because
    it is used in test cases.
    
    Closes #13, fixes #12.

--- src/WhoopsRunner.php
+++ src/WhoopsRunner.php
@@ -2,7 +2,7 @@
 
 namespace Franzl\Middleware\Whoops;
 
-use Laminas\Diactoros\Response\HtmlResponse;
+use Middlewares\Utils\Factory;
 use Psr\Http\Message\ServerRequestInterface;
 use Whoops\Handler\PlainTextHandler;
 use Whoops\Run;
@@ -21,13 +21,11 @@ class WhoopsRunner
 
         ob_start();
         $whoops->$method($error);
-        $response = ob_get_clean();
+        $content = ob_get_clean();
 
-        return new HtmlResponse(
-            $response,
-            500,
-            ['Content-Type' => $format->getPreferredContentType()]
-        );
+        return Factory::createResponse(500)
+            ->withBody(Factory::createStream($content))
+            ->withHeader('Content-Type', $format->getPreferredContentType());
     }
 
     private static function getWhoopsInstance(Formats\Format $format)
